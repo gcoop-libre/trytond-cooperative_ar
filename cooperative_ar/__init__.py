ce2c14c60a7c50f4889fb7fdca9071bfa642f471
Joaquín Sorianello <joac@gcoop.coop>
2013-06-25 16:36:40 -0300

    Agrego campo 'condicion ante el iva' en Party

--- cooperative_ar/__init__.py
+++ cooperative_ar/__init__.py
@@ -3,7 +3,8 @@ from .partner import *
 from .meeting import *
 from .vacation import *
 from .partnermeeting import *
+from .party import *
 
 
 def register():
-    Pool.register(Partner, Meeting, Vacation, PartnerMeeting, module='cooperative_ar', type_='model')
+    Pool.register(Partner, Meeting, Vacation, PartnerMeeting, Party, module='cooperative_ar', type_='model')
---
+++ cooperative_ar/party.py
@@ -0,0 +1,24 @@
+#! -*- coding: utf8 -*-
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.pyson import Eval, Equal
+
+__all__ = ['Party']
+
+class Party(ModelSQL, ModelView):
+    """Pary module, extended for cooperative_ar"""
+
+    __name__ = 'party.party'
+
+    iva_condition = fields.Selection(
+            [
+                ('responsable_inscripto', 'Responsable Inscripto'),
+                ('exento', 'Exento'),
+                ('consumidor_final', 'Consumidor Final'),
+            ],
+            'Condicion ante el IVA',
+            states={
+                'readonly': ~Eval('active', True),
+                'required': Equal(Eval('vat_country'), 'AR'),
+                },
+
+            )
