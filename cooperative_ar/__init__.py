cc3bb7d8542ed361ef51967effec7cc5647491e3
diego <diego@gcoop.coop>
2013-08-06 15:43:19 -0300

    Argentina is the country default

--- cooperative_ar/__init__.py
+++ cooperative_ar/__init__.py
@@ -4,7 +4,8 @@ from .meeting import *
 from .vacation import *
 from .partnermeeting import *
 from .party import *
+from .address import *
 
 
 def register():
-    Pool.register(Partner, Meeting, Vacation, PartnerMeeting, Party, module='cooperative_ar', type_='model')
+    Pool.register(Partner, Meeting, Vacation, PartnerMeeting, Party, Address, module='cooperative_ar', type_='model')
