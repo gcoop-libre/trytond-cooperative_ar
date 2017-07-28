c496e0cf5601a39eabb04fc946612380c3641c30
Luciano Rossi <lukio@gcoop.coop>
2017-07-28 14:44:50 -0300

    fixes #8 Menu de configuracion para parametrizar el módulo

---
+++ configuration.py
@@ -0,0 +1,28 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
+from trytond.pyson import Eval, Bool
+
+__all__ = ['Configuration']
+
+
+class Configuration(ModelSingleton, ModelSQL, ModelView):
+    'Cooperative Configuration'
+    __name__ = 'cooperative_ar.configuration'
+
+    receipt_account_payable = fields.Property(fields.Many2One('account.account',
+            'Account Payable', domain=[
+                ('company', '=', Eval('context', {}).get('company', -1)),
+                ],
+            states={
+                'required': Bool(Eval('context', {}).get('company')),
+                'invisible': ~Eval('context', {}).get('company'),
+                }))
+    receipt_account_receivable = fields.Property(fields.Many2One('account.account',
+            'Account Receivable', domain=[
+                ('company', '=', Eval('context', {}).get('company', -1)),
+                ],
+            states={
+                'required': Bool(Eval('context', {}).get('company')),
+                'invisible': ~Eval('context', {}).get('company'),
+                }))
