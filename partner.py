3b0cefc5253631a438dd08eeb8d8e7fbea0572b4
Luciano Rossi <lukio@gcoop.coop>
2017-08-15 15:08:43 -0300

    fixes #22 Add pep8
    Conflicts:
    	account.py

--- partner.py
+++ partner.py
@@ -1,52 +1,60 @@
 #! -*- coding: utf8 -*-
+# This file is part of the cooperative_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Equal, Eval, Id
 from trytond.transaction import Transaction
 
 __all__ = ['Partner']
 
+
 class Partner(ModelSQL, ModelView):
-    "cooperative_ar"
-    __name__ = "cooperative.partner"
+    'Partner'
+    __name__ = 'cooperative.partner'
     _rec_name = 'party'
-    status = fields.Selection([('active', 'Active'),
-                               ('give_up', 'Give Up'),
-                               ],'Status', required=True)
+    status = fields.Selection([
+        ('active', 'Active'),
+        ('give_up', 'Give Up'),
+        ], 'Status', required=True)
     file = fields.Integer('File', required=True)
     party = fields.Many2One('party.party', 'Party', required=True)
     company = fields.Many2One('company.company', 'Company', required=True)
     first_name = fields.Char('First Name', required=True)
     last_name = fields.Char('Last Name', required=True)
-    gender = fields.Selection([('male', 'Male'),
-                               ('female', 'Female'),
-                               ('other', 'other'),
-                               ], 'Gender', required=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'other'),
+        ], 'Gender', required=True)
     dni = fields.Char('DNI', required=True)
-    nationality = fields.Many2One('country.country', 'Nationality', required=True)
-    marital_status = fields.Selection([('soltero/a', 'Soltero/a'),
-                                       ('casado/a', 'Casado/a'),
-                                       ('divorciado/a', 'Divorciado/a'),
-                                       ('viudo/a', 'Viudo/a'),
-                                       ('otra', 'Otra'),
-                                       ], 'Marital Status', required=True)
+    nationality = fields.Many2One('country.country', 'Nationality',
+        required=True)
+    marital_status = fields.Selection([
+        ('soltero/a', 'Soltero/a'),
+        ('casado/a', 'Casado/a'),
+        ('divorciado/a', 'Divorciado/a'),
+        ('viudo/a', 'Viudo/a'),
+        ('otra', 'Otra'),
+        ], 'Marital Status', required=True)
     incorporation_date = fields.Date('Incorporation Date', required=True)
-    leaving_date = fields.Date('Leaving Date',
-            states={
+    leaving_date = fields.Date('Leaving Date', states={
                 'readonly': ~Equal(Eval('status'), 'give_up'),
                 'required': Equal(Eval('status'), 'give_up'),
-                },
-            )
-
+                })
     payed_quotes = fields.Numeric('Payed Quotes')
     vacation_days = fields.Integer('Vacation Days')
-    vacation = fields.One2Many('cooperative.partner.vacation', 'partner', 'Vacation')
-    meeting = fields.Many2Many('cooperative.partner-meeting', 'partner', 'meeting', 'Meeting')
-    sanction = fields.One2Many('cooperative.partner.sanction', 'partner', 'Sanction')
+    vacation = fields.One2Many('cooperative.partner.vacation', 'partner',
+        'Vacation')
+    meeting = fields.Many2Many('cooperative.partner-meeting', 'partner',
+        'meeting', 'Meeting')
+    sanction = fields.One2Many('cooperative.partner.sanction', 'partner',
+        'Sanction')
     recibo = fields.One2Many('cooperative.partner.recibo', 'partner', 'Recibo')
     proposal_letter = fields.Binary('Proposal Letter')
     proof_tax = fields.Binary('Proof of tax registation')
-
-    meeting_date_of_incoroporation = fields.Date('Meeting date of incorporation', required=True)
+    meeting_date_of_incoroporation = fields.Date(
+        'Meeting date of incorporation', required=True)
     birthdate = fields.Date('Birthdate', required=True)
 
     def get_rec_name(self, name):
@@ -72,9 +80,16 @@ class Partner(ModelSQL, ModelView):
         return Transaction().context.get('company')
 
     @classmethod
+    def __setup__(cls):
+        super(Partner, cls).__setup__()
+        cls._error_messages.update({
+            'unique_file': 'The file must be unique.',
+            })
+
+    @classmethod
     def write(cls, partners, vals):
         if 'file' in vals:
-            data = cls.search([('file','=', vals['file'])])
+            data = cls.search([('file', '=', vals['file'])])
             if data and data != partners:
                 cls.raise_user_error('unique_file')
 
@@ -84,15 +99,8 @@ class Partner(ModelSQL, ModelView):
     def create(cls, vlist):
         for vals in vlist:
             if 'file' in vals:
-                data = cls.search([('file','=', vals['file'])])
+                data = cls.search([('file', '=', vals['file'])])
                 if data:
                     cls.raise_user_error('unique_file')
 
         return super(Partner, cls).create(vlist)
-
-    @classmethod
-    def __setup__(cls):
-        super(Partner, cls).__setup__()
-        cls._error_messages.update({
-            'unique_file': 'The file must be unique.',
-            })
--- partnermeeting.py
+++ partnermeeting.py
@@ -1,11 +1,15 @@
 #! -*- coding: utf8 -*-
-from trytond.model import ModelView, ModelSQL, fields
+# This file is part of the cooperative_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import ModelSQL, fields
 
 __all__ = ['PartnerMeeting']
 
+
 class PartnerMeeting(ModelSQL):
-    "cooperative_ar"
-    __name__ = "cooperative.partner-meeting"
+    'Partner Meeting'
+    __name__ = 'cooperative.partner-meeting'
+
     partner = fields.Many2One('cooperative.partner', 'Partner')
     meeting = fields.Many2One('cooperative.meeting', 'Meeting')
-
